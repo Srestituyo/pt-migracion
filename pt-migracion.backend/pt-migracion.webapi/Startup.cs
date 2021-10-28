d397de746a43e7cb6c8a891ed7ccd85b4065a4e2
Srestituyo <sebastianrestituyo@gmail.com>
2021-10-28 09:21:08 -0400

    Adding Estado Initial Data

--- pt-migracion.backend/pt-migracion.webapi/Startup.cs
+++ pt-migracion.backend/pt-migracion.webapi/Startup.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -5,6 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using pt_migracion.data;
+using pt_migracion.data.Entity;
+using System;
 
 namespace pt_migracion.webapi
 {
@@ -16,6 +19,34 @@ namespace pt_migracion.webapi
             using (var client = new ApplicationDbContext())
             {
                 client.Database.EnsureCreated();
+                 
+                if (!client.Estados.Any())
+                {
+                    var aEstado = new Estados()
+                    {
+                        Estado = "Abiertas",
+                        TimeStamp = DateTime.UtcNow
+                    };
+
+                    client.Estados.Add(aEstado);
+
+                    var aEstado1 = new Estados()
+                    {
+                        Estado = "Aprobadas",
+                        TimeStamp = DateTime.UtcNow
+                    };
+
+                    client.Estados.Add(aEstado1);
+
+                    var aEstado2 = new Estados()
+                    {
+                            Estado = "Canceladas",
+                            TimeStamp = DateTime.UtcNow
+                    };
+
+                    client.Estados.Add(aEstado2); 
+                    client.SaveChanges();
+                } 
             }
         }
 
